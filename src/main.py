b604aa592232605bfd1ea4c6fe3de985b5650f83
46891822+simigami@users.noreply.github.com <46891822+simigami@users.noreply.github.com>
2024-10-29 18:05:09 +0900

    add: naming wandb and directory

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
-import datetime
 import os
+os.environ["CUBLAS_WORKSPACE_CONFIG"] = ":4096:8"
+
 import random
 import numpy as np
 import pandas as pd
@@ -7,7 +8,6 @@ from tqdm import tqdm
 import wandb
 import sys
 
-os.environ["CUBLAS_WORKSPACE_CONFIG"] = ":4096:8"
 import torch
 
 import evaluate
@@ -22,8 +22,6 @@ from sklearn.model_selection import train_test_split
 from model import BERTDataset
 from arguments import ModelArguments, DataTrainingArguments
 
-# for wandb setting
-# os.environ['WANDB_DISABLED'] = 'true'
 os.environ["WANDB_MODE"] = "online"
 logger = logging.getLogger(__name__)
 
@@ -41,17 +39,7 @@ SEED = 456
 set_seed(SEED)
 DEVICE = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
 
-# BASE_DIR is parent directory of src
-BASE_DIR = os.path.dirname(os.getcwd())
-DATA_DIR = os.path.join(BASE_DIR, 'data')
-MODEL_DIR = os.path.join(BASE_DIR, 'model')
-OUTPUT_DIR = os.path.join(BASE_DIR, 'output')
-
-os.makedirs(DATA_DIR, exist_ok=True)
-os.makedirs(OUTPUT_DIR, exist_ok=True)
-os.makedirs(MODEL_DIR, exist_ok=True)
-
-def main():
+def main(run_name):
     parser = HfArgumentParser(
             (ModelArguments, DataTrainingArguments, TrainingArguments)
         )
@@ -59,9 +47,9 @@ def main():
 
     project_prefix = "[train]" if train_args.do_train else "[eval]" if train_args.do_eval else "[pred]"
     wandb.init(
-        project="odqa",
+        project="data_centric",
         entity="nlp15",
-        name=f"{project_prefix} {model_args.model_name_or_path.split('/')[0]}_{data_args.dataset_name}_{(datetime.datetime.now() + datetime.timedelta(hours=9)).strftime('%Y%m%d_%H%M%S')}",
+        name=f"{project_prefix}_{run_name}",
         save_code=True,
     )
 
@@ -74,7 +62,6 @@ def main():
     logging.info(f"model is from {model_args.model_name_or_path}")
     logging.info(f"data is from {data_args.dataset_name}")
 
-
     # Load model
     if train_args.do_train:
         model_name = model_args.model_name_or_path #'klue/bert-base'
@@ -82,6 +69,7 @@ def main():
     if not train_args.do_train:
         model_name = os.path.join(model_args.model_name_or_path, 'checkpoint-124')
         tokenizer = AutoTokenizer.from_pretrained('klue/bert-base')
+
     model = AutoModelForSequenceClassification.from_pretrained(model_name, num_labels=7).to(DEVICE)
 
     # Load data
@@ -91,18 +79,6 @@ def main():
     data_train = BERTDataset(dataset_train, tokenizer, data_args)
     data_valid = BERTDataset(dataset_valid, tokenizer, data_args)
     data_collator = DataCollatorWithPadding(tokenizer=tokenizer)
-
-    # Training arguments
-    if train_args.do_train:
-        run_name = input('Please Enter Your Run Name : ')
-
-        while run_name == '':
-            run_name = input('Please Enter Your Run Name : ')
-
-        run_name += datetime.datetime.now().strftime('_%Y%m%d_%H%M%S')
-
-    # os.makedirs(os.path.join(MODEL_DIR, run_name), exist_ok=True)
-    # os.makedirs(os.path.join(OUTPUT_DIR, run_name), exist_ok=True)
 
     if train_args.do_train:
         model = train(model, data_train, data_valid, data_collator, run_name, train_args)
@@ -121,7 +97,7 @@ def train(model, data_train, data_valid, data_collator, run_name : str, train_ar
         logging_strategy='steps',
         eval_strategy='steps',
         save_strategy='steps',
-        logging_steps=100,
+        logging_steps=10,
         eval_steps=100,
         save_steps=100,
         save_total_limit=2,
@@ -164,7 +140,6 @@ def train(model, data_train, data_valid, data_collator, run_name : str, train_ar
     return model
 
 def predict(model, tokenizer, train_args: TrainingArguments, data_args: DataTrainingArguments = None, run_name : str = None):
-    # output_dir = os.path.join(OUTPUT_DIR, run_name)
     dataset_test = pd.read_csv(data_args.test_dataset_name) #pd.read_csv(os.path.join(DATA_DIR, 'test.csv'))
     model.eval()
     preds = []
@@ -176,8 +151,18 @@ def predict(model, tokenizer, train_args: TrainingArguments, data_args: DataTrai
             pred = torch.argmax(torch.nn.Softmax(dim=1)(logits), dim=1).cpu().numpy()
             preds.extend(pred)
     dataset_test['target'] = preds
-    dataset_test.to_csv(train_args.output_dir, index=False) 
-    #dataset_test.to_csv(os.path.join(output_dir, 'output.csv'), index=False)
+
+    os.makedirs(train_args.output_dir, exist_ok=True)
+    dataset_test.to_csv(os.path.join(train_args.output_dir, 'predictions.csv'), index=False)
 
 if __name__ == '__main__':
-    main()
+    try:
+        argv_run_index = sys.argv.index('--run_name') + 1
+        argv_run_name = sys.argv[argv_run_index]
+
+    except ValueError:
+        argv_run_name = ''
+        while argv_run_name == '':
+            argv_run_name = input("run name is missing, please add run name : ")
+
+    main(argv_run_name)
